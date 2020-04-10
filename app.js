9884748096d345cbd47d5d226107eb739d07932e
Yahaya <yasholma1@gmail.com>
2020-04-10 21:13:56 +0100

    Commit

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(router);
 
-const PORT = 5000;
+const PORT = process.env.PORT
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
